10e67510452d5c68a83566c44c90ccdf31f3ae71
Yevhen Savenko <eugene.savenko@regulus.team>
2021-05-04 17:48:50 +0300

    End^C task 5

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -11,9 +11,18 @@ class ProductController extends Controller
     public function actionIndex()
     {
         $product = new Product();
-        $productReturned = $product->sortProduct();
+        $productReturned = $product->getSortedProducts();
 
         $view = new View('task2');
         $view->render('task2', $productReturned);
+    }
+
+    public function actionSort()
+    {
+        $product = new Product();
+        $productReturned = $product->sortProductWithDiscount();
+
+        $view = new View('task5');
+        $view->render('task5', $productReturned);
     }
 }
--- app/Views/tasks/task2.php
+++ app/Views/tasks/task2.php
@@ -1,26 +1,4 @@
-<div class="row justify-content-center">
-    <div class="dropdown col-md-3">
-        <a class="btn btn-secondary dropdown-toggle py-2 px-4 mb-3" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-            <?php if (isset($_GET['sort']) && isset($_GET['order'])) : ?>
-                <?php switch ($_GET['order']):
-                    case '0':
-                        echo 'Від дешевших до дорожчих';
-                        break;
-                    case '1':
-                        echo 'Від дорожчих до дешевших';
-                        break;
-                endswitch ?>
-            <?php else : ?>
-                <?= 'Виберіть тип сортування' ?>
-            <?php endif ?>
-        </a>
-
-        <ul class="dropdown-menu py-2 px-4" aria-labelledby="dropdownMenuLink">
-            <a class="dropdown-item" href="<?php $_SERVER['PHP_SELF'] ?>?sort=price&order=0">Від дешевших до дорожчих</a>
-            <a class="dropdown-item" href="<?php $_SERVER['PHP_SELF'] ?>?sort=price&order=1">Від дорожчих до дешевших</a>
-        </ul>
-    </div>
-</div>
+<?php require_once ROOT . '/app/Views/tasks/general/sort.php' ?>
 
 <div class="row justify-content-center">
     <div class="col-md-6">
@@ -35,7 +13,7 @@
                     </span> грн</p>
                 <p>
                     <?php if (!$product['qty'] > 0) : ?>
-                        echo 'Нема в наявності';
+                        <?= 'Нема в наявності' ?>
                     <?php endif ?>
                 </p>
             </div>
